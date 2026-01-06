63455e0c94b9f20a8084bc80e25b190ab92a09d5
smaran pinisetty <smaranpinisetty@gmail.com>
2026-01-06 02:26:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'plus.unsplash.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
